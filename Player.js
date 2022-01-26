3d9ac2dce21fd34c180abcaf5dfea576808fb38c
CREATIVE-3800X\michi <web@michaelholzner.ch>
2022-01-26 10:34:57 +0100

    First Version to try out

--- Player.js
+++ Player.js
@@ -4,7 +4,46 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    const currentBuyIn = gameState["current_buy_in"];
+    const minimumRaise = gameState["minimum_raise"];
+
+    const playerId = gameState["in_action"];
+    const currentPlayer = gameState["players"][playerId];
+    const currentHand = currentPlayer["hole_cards"];
+
+    let betAmount = 0;
+
+    switch(currentHand){
+      case currentHand[0]["rank"] === "A" &&  currentHand[1]["rank"] === "A":
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+      case currentHand[0]["rank"] === "K" &&  currentHand[1]["rank"] === "K":
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+      case currentHand[0]["rank"] === "Q" &&  currentHand[1]["rank"] === "Q":
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+      case currentHand[0]["rank"] === "J" &&  currentHand[1]["rank"] === "J":
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+      case (currentHand[0]["rank"] === "A" && currentHand[1]["rank"] === "K") || (currentHand[0]["rank"] === "K" &&  currentHand[1]["rank"] === "A"):
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+      case (currentHand[0]["rank"] === "A" && currentHand[1]["rank"] === "Q") || (currentHand[0]["rank"] === "Q" &&  currentHand[1]["rank"] === "A"):
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+        case (currentHand[0]["rank"] === "A" && currentHand[1]["rank"] === "J") || (currentHand[0]["rank"] === "J" &&  currentHand[1]["rank"] === "A"):
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+      case (currentHand[0]["rank"] === "K" && currentHand[1]["rank"] === "Q") || (currentHand[0]["rank"] === "Q" &&  currentHand[1]["rank"] === "K"):
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+        case currentHand[0]["rank"] === currentHand[1]["rank"]:
+        betAmount = currentBuyIn - currentPlayer["bet"] + minimumRaise;
+        break;
+    }
+
+    bet(betAmount);
   }
 
   static showdown(gameState) {
